41dd900bbb2884660978ad74b34e9cfbb0fcc87e
Milos Zivlak <zivlakmilos@gmail.com>
2016-03-05 00:30:19 +0100

    Uradjena osnova za klasu HandGesture

--- interface/handgesture.cpp
+++ interface/handgesture.cpp
@@ -1,5 +1,17 @@
 #include "handgesture.h"
 
+using namespace std;
+using namespace cv;
+
 HandGesture::HandGesture()
+{
+    this->cap = VideoCapture(0);
+}
+
+void HandGesture::pickColor()
+{
+}
+
+void HandGesture::trackHand(int *rot, int *seg1, int *seg2, int *seg3)
 {
 }
--- interface/handgesture.h
+++ interface/handgesture.h
@@ -1,13 +1,40 @@
 #ifndef HANDGESTURE_H
 #define HANDGESTURE_H
 
+#include <vector>
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+typedef struct
+{
+    int x;
+    int y;
+    int w;
+    int h;
+} Rect;
 
 class HandGesture
 {
 public:
     HandGesture();
+
+    void pickColor();
+    void trackHand(int *rot, int *seg1, int *seg2, int *seg3);
+
+private:
+    enum { RectLeanght = 20 };
+
+    cv::VideoCapture cap;
+    cv::Mat original;
+    cv::Mat tresholded;
+
+    int huleL;
+    int huleH;
+    int saturationL;
+    int saturationH;
+    int valueL;
+    int valueH;
 };
 
 #endif // HANDGESTURE_H
